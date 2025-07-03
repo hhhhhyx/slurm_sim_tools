9c6d42309e0a7578be5fa7b77fd4c4da31665f27
Yuxuan He <2990346h@student.gla.ac.uk>
2025-07-03 16:14:16 +0100

    some configurations and prepared work

---
+++ tutorials/micro_cluster/smallrun.sh
@@ -0,0 +1,15 @@
+export PATH=/opt/slurm_sim_tools/bin:$PATH
+
+
+export CLUS_DIR=$(pwd)
+export MACHINE_NAME="slurmsimcont"
+export RUN_NAME="priority_saf"
+export dtstart=79
+export replica=1
+
+slurmsim -v run_sim  -d \
+            -e ${CLUS_DIR}/etc \
+            -a ${CLUS_DIR}/etc/sacctmgr.script \
+            -w ${CLUS_DIR}/workload/small.events \
+            -r ${CLUS_DIR}/results/${MACHINE_NAME}/${RUN_NAME}/dtstart_${dtstart}_${replica} \
+            -dtstart $dtstart
